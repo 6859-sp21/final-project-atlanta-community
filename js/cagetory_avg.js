1e3cb103bdbf92618b190d5c6b431dc5f5ce8cb8
ahorne21 <83245199+ahorne21@users.noreply.github.com>
2021-04-28 15:21:00 -0400

    Update cagetory_avg.js

--- js/cagetory_avg.js
+++ js/cagetory_avg.js
@@ -1,10 +1,38 @@
-d3.csv("rent.csv", ready);
+var margin = {top: 10, right: 10, bottom: 100, left: 60};
+
+var width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0,width],1);
+
+var x2 = d3.scale.ordinal()
+    .rangeRoundBands([0,width],0);
+
+var y = d3.scale.linear() 
+    .range([height,0]); 
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var color = d3.scale.category10();
+
+var tooltip = d3.select("body").append("div")   
+        .attr("class", "tooltip")               
+        .style("opacity", 0);
+
+d3.csv("category_avg.csv", ready);
 
 function ready(error, data) {
     if (error) throw error;
 
     data.forEach(function(d) {
-        d.rent = +d.Rent;
+        d.alc = +d.avg_lex_change;
     });
 
     console.log("data", data);
@@ -15,9 +43,9 @@ function ready(error, data) {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    x.domain(data.map(function(d) { return d.City; }));
-    y.domain([0, d3.max(data, function(d) { return d.rent; })]);
-    x2.domain(data.map(function(d) { return d.City; }));
+    x.domain(data.map(function(d) { return d.category; }));
+    y.domain([0, d3.max(data, function(d) { return d.alc; })]);
+    x2.domain(data.map(function(d) { return d.category; }));
 
     svg.append("g")
         .attr("class", "axis axis--x")
@@ -38,7 +66,7 @@ function ready(error, data) {
         .attr("y", 6)
         .attr("dy", "0em")
         .attr("text-anchor", "end")
-        .text("Rent ($)");
+        .text("Average Lexical Change");
 
     bars = svg.append("g").attr("class", "bars");
 
@@ -46,14 +74,14 @@ function ready(error, data) {
         .data(data)
         .enter().append("rect")
         .attr("class", "bar")
-        .attr("x", function(d) { return x(d.City); })
-        .attr("y", function(d) { return y(d.rent); })
+        .attr("x", function(d) { return x(d.category); })
+        .attr("y", function(d) { return y(d.alc); })
         .attr("width", 10)
-        .attr("height", function(d) { return height - y(d.rent); })
+        .attr("height", function(d) { return height - y(d.alc); })
         .style("fill", "#ccc")
         .on("mouseover", function(d) {
-            d3.select(this).style("fill", function(d) { return color(d.City); })
-            tooltip.text(d.City + ", " + d.State + " $" + d.rent)
+            d3.select(this).style("fill", function(d) { return color(d.category); })
+            tooltip.text(d.category + " " + d.alc)
             .style("opacity", 0.8)
                     .style("left", (d3.event.pageX)+0 + "px") 
                     .style("top", (d3.event.pageY)-0 + "px");
@@ -65,11 +93,11 @@ function ready(error, data) {
         });
 
     
-    var sum = d3.sum(data, function(d) { return d.rent; }); 
+    var sum = d3.sum(data, function(d) { return d.alc; }); 
     var average = sum/data.length;
 
     var line = d3.svg.line()
-        .x(function(d, i) { return x2(d.City) + i; })
+        .x(function(d, i) { return x2(d.category) + i; })
         .y(function(d, i) { return y(average); }); 
 
     svg.append("path")
