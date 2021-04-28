107dee3c57ab130ef10e687e70f65e03d4cb5301
ahorne21 <83245199+ahorne21@users.noreply.github.com>
2021-04-28 15:44:58 -0400

    Update categoryAvgBarGraph.js

--- js/categoryAvgBarGraph.js
+++ js/categoryAvgBarGraph.js
@@ -26,90 +26,92 @@ var tooltip = d3.select("body").append("div")
         .attr("class", "tooltip")               
         .style("opacity", 0);
 
+function showBarGraphh(){
 //d3.csv("category_avg.csv", ready);
-d3.csv("./data/category_avg.csv", function(error, data) {
-    if (error) throw error;
+    d3.csv("./data/category_avg.csv", function(error, data) {
+        if (error) throw error;
 
-    data.forEach(function(d) {
-        d.alc = +d.avg_lex_change;
-    });
-
-    console.log("data", data);
-      
-    var svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    x.domain(data.map(function(d) { return d.category; }));
-    y.domain([0, d3.max(data, function(d) { return d.alc; })]);
-    x2.domain(data.map(function(d) { return d.category; }));
-
-    svg.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis)
-        .selectAll("text")
-        .attr("y", 0)
-        .attr("x", 9)
-        .attr("dy", "-0.2em")
-        .attr("transform", "rotate(90)")
-        .style("text-anchor", "start");
-
-    svg.append("g")
-        .attr("class", "axis axis--y")
-        .call(yAxis)
-        .append("text")
-        //.attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0em")
-        .attr("text-anchor", "end")
-        .text("Average Lexical Change");
-
-    bars = svg.append("g").attr("class", "bars");
-
-    bars.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.category); })
-        .attr("y", function(d) { return y(d.alc); })
-        .attr("width", 10)
-        .attr("height", function(d) { return height - y(d.alc); })
-        .style("fill", "#ccc")
-        .on("mouseover", function(d) {
-            d3.select(this).style("fill", function(d) { return color(d.category); })
-            tooltip.text(d.category + " " + d.alc)
-            .style("opacity", 0.8)
-                    .style("left", (d3.event.pageX)+0 + "px") 
-                    .style("top", (d3.event.pageY)-0 + "px");
-        })
-        .on("mouseout", function(d) {
-            tooltip.style("opacity", 0);
-            d3.select(this).style("fill", "#ccc");
-
+        data.forEach(function(d) {
+            d.alc = +d.avg_lex_change;
         });
 
+        console.log("data", data);
+      
+        var svg = d3.select("body").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
     
-    var sum = d3.sum(data, function(d) { return d.alc; }); 
-    var average = sum/data.length;
+        x.domain(data.map(function(d) { return d.category; }));
+        y.domain([0, d3.max(data, function(d) { return d.alc; })]);
+        x2.domain(data.map(function(d) { return d.category; }));
 
-    var line = d3.svg.line()
-        .x(function(d, i) { return x2(d.category) + i; })
-        .y(function(d, i) { return y(average); }); 
+        svg.append("g")
+            .attr("class", "axis axis--x")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+            .attr("y", 0)
+            .attr("x", 9)
+            .attr("dy", "-0.2em")
+            .attr("transform", "rotate(90)")
+            .style("text-anchor", "start");
 
-    svg.append("path")
-        .datum(data)
-        .attr("class", "mean")
-        .attr("d", line);
+        svg.append("g")
+            .attr("class", "axis axis--y")
+            .call(yAxis)
+            .append("text")
+            //.attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0em")
+            .attr("text-anchor", "end")
+            .text("Average Lexical Change");
 
-    svg.append("text")
-        .attr("transform", "translate(" + (width+3) + "," + y(average) + ")")
-        .attr("dy", "1em")
-        .attr("text-anchor", "end")
-        .style("fill", "red")
-        .html("Average = $" + average);
+        bars = svg.append("g").attr("class", "bars");
+
+        bars.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.category); })
+            .attr("y", function(d) { return y(d.alc); })
+            .attr("width", 10)
+            .attr("height", function(d) { return height - y(d.alc); })
+            .style("fill", "#ccc")
+            .on("mouseover", function(d) {
+                d3.select(this).style("fill", function(d) { return color(d.category); })
+                tooltip.text(d.category + " " + d.alc)
+                .style("opacity", 0.8)
+                        .style("left", (d3.event.pageX)+0 + "px") 
+                        .style("top", (d3.event.pageY)-0 + "px");
+            })
+            .on("mouseout", function(d) {
+                tooltip.style("opacity", 0);
+                d3.select(this).style("fill", "#ccc");
+
+            });
+
+    
+        var sum = d3.sum(data, function(d) { return d.alc; }); 
+        var average = sum/data.length;
+
+        var line = d3.svg.line()
+            .x(function(d, i) { return x2(d.category) + i; })
+            .y(function(d, i) { return y(average); }); 
+
+        svg.append("path")
+            .datum(data)
+            .attr("class", "mean")
+            .attr("d", line);
+
+        svg.append("text")
+            .attr("transform", "translate(" + (width+3) + "," + y(average) + ")")
+            .attr("dy", "1em")
+            .attr("text-anchor", "end")
+            .style("fill", "red")
+            .html("Average = $" + average);
 
 
+    }
 }
