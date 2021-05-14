daa1b512cb6606284737f70d1ac828906ddc1a1e
ahorne21 <83245199+ahorne21@users.noreply.github.com>
2021-05-14 11:40:21 -0400

    Update Intro.vue

--- app/src/views/Intro.vue
+++ app/src/views/Intro.vue
@@ -11,8 +11,11 @@
     <div id="content">
       <div id="text-block" v-scrollanimation>
         <p>
-        Our team will explore how different communities in Atlanta perceive language in different ways. To do this, we’ll be using a dataset of 149 local communities with information about their online behavior on Twitter.  Information at the twitter account level allowed us to create summary statistics at the community level such as  community size,  average number of followers, friends and tweets and understand what are the top follows and topic words in each community. Following a specific methodology, deviations from the normal language can be measured and these deviations are called lexical variation. 
-        Our main goal is that the user can understand lexical variation in social media across Atlanta’s communities and have a guide for current local trends online. Examples of potential users  are journalists, politicians and society as a whole.
+        Our team will explore how different communities in Atlanta perceive language in different ways. To do this, we’ll be using a dataset of 149 local communities with information about their online behavior on Twitter.  Information at the twitter account level allowed us to create summary statistics at the community level such as  community size,  average number of followers, friends and tweets and understand what are the top follows and topic words in each community. Following a specific methodology, deviations from the normal language can be measured and these deviations are called semantic change. 
+        Our main goal is that the user can understand the semantic change in social media across Atlanta’s communities and have a guide for current local trends online. Examples of potential users  are journalists, politicians and society as a whole.
+        </p>
+        <p>
+        What is Semantic Change? According to wikipedia, Semantic Change is a form of language change regarding the evolution of word usage. For example, if you take the word "Gay", that word's usage has drastically changed over the past century. Originally in the 1940s, "Gay" meant happy and joyful. But today, the word "Gay" is associated with the LGBTQIA+ community. Another example is the phrase "What's up?". The original meaning of that in English would've meant "what is located above me?", but today it's used as a casual way of starting conversation. 
         </p>
       </div> 
       <div id="image-block" v-scrollanimation>
