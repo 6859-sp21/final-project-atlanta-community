7bd83b36f6edce2e92c61af78925fdf2062efe2a
Yuebin Dong <yuebindo@mit.edu>
2021-05-19 01:12:48 -0400

    Adjusted text

--- app/src/views/Project.vue
+++ app/src/views/Project.vue
@@ -20,7 +20,7 @@
         <div class="content">
           <h1>Exploring patterns of language variation on social media</h1>
           <h2>A case study in Atlanta online communities</h2>
-          <p>Team: Yuebin Dong, Jiang Hang, Amanda Horne, Diego Lestani
+          <p>Team: Yuebin Dong, Jiang Hang, Diego Lestani, Amanda Horne
           </p>
           <p> Warning: there may be some explicit word usage not appropriate for all ages on this website</p>
           <b-button class="button-blue" href="/final-project-atlanta-community/#/viz" variant="primary">Go to Our App</b-button>
